e5985272fd46b474bef0b2153e4e66a159d825c5
Liisa Lahti <liisa.r.lahti@gmail.com>
2023-11-01 09:36:15 +0200

    Use react-query to fetch stations

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import { useQuery } from "react-query"
 
-function App() {
-  const [count, setCount] = useState(0)
+interface Station {
+  id: number
+  station_name: string
+  station_address: string
+  coordinate_x: string
+  coordinate_y: string
+}
+
+const App = () => {
+  const getStations = async () => {
+    const response: Response = await fetch(`http://localhost:3001/api/station`)
+    const data = await response.json()
+    return data
+  }
+
+  const { data, status } = useQuery(["stations"], () => getStations())
+
+  const stations = status === "success" && data
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>🚲 Citybike stats 🚲</h1>
+      <ul>
+        {stations.map(({ id, station_name }: Station) => (
+          <li key={id}>
+            {id} {station_name}
+          </li>
+        ))}
+      </ul>
+    </main>
   )
 }
 
