78268ee3ee5e4a18ef0c4c6c3299923bbc5c8a63
Liisa Lahti <liisa.r.lahti@gmail.com>
2023-11-02 11:37:46 +0200

    Style station list

--- src/components/StationList.tsx
+++ src/components/StationList.tsx
@@ -1,19 +1,39 @@
 import { FC } from "react"
 import { Station } from "../hooks/useStationList"
+import { styled } from "styled-components"
 
 type StationListProps = {
   stations: Station[]
 }
 
 const StationList: FC<StationListProps> = ({ stations }) => (
-  <ul>
+  <List>
     {stations &&
       stations.map(({ id, station_name }: Station) => (
-        <li key={id}>
-          {id} {station_name}
-        </li>
+        <a href={`/station/${id}`} key={id}>
+          <ListItem >
+            {id} – {station_name}
+          </ListItem>
+        </a>
       ))}
-  </ul>
+  </List>
 )
 
 export default StationList
+
+const List = styled.ul`
+  list-style: none;
+  text-align: left;
+  padding: 4px;
+  margin: 0;
+`
+
+const ListItem = styled.li`
+  padding: 8px 16px;
+  border-radius: 6px;
+
+  &:hover {
+    background: lavender;
+    cursor: pointer;
+  }
+`
