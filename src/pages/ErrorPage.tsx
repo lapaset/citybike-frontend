c0b22922877936ec13dfa55ede92c23c036ee82a
Liisa Lahti <liisa.r.lahti@gmail.com>
2023-11-02 14:18:59 +0200

    Style error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,16 +1,37 @@
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import { styled } from "styled-components";
 
 const ErrorPage = () => {
   const error = useRouteError();
 
   const errorText = isRouteErrorResponse(error) ? error.statusText : error instanceof Error ? error.message : ''
 
-  return <main>
+  return <Container>
     <h1>Oops!</h1>
     <p>Sorry, an error has occured</p>
     <p><em>{errorText}</em></p>
     <a href='/'>Back to the frontpage</a>
-  </main>
+  </Container>
 }
 
 export default ErrorPage
+
+const Container = styled.main`
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  background: white;
+  border-radius: 8px;
+  margin: 24px;
+  padding: 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  a {
+    margin: 36px;
+  }
+
+  p {
+    margin: 0;
+  }
+`
