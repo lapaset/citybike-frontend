40a39fc37faf0b09721b77edf778be650b45276b
Liisa Lahti <liisa.r.lahti@gmail.com>
2023-11-02 14:18:50 +0200

    Throw errors when getting station info

--- src/api/station-info.ts
+++ src/api/station-info.ts
@@ -1,10 +1,15 @@
 export const getStationInfo = async (id?: string) => {
   if (id === undefined || isNaN(parseInt(id))) {
-    return null
+    throw new Error("Invalid id")
   }
   const response: Response = await fetch(
     `http://localhost:3001/api/station/${id}`
   )
+
+  if (response.status === 404) {
+    throw new Error("404 Data not found")
+    return
+  }
   const data = await response.json()
   return data
 }
