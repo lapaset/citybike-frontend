b8d0cfa2b899ca7e1820f4f43f87741832cc3ffa
Liisa Lahti <liisa.r.lahti@gmail.com>
2023-11-03 12:40:07 +0200

    Fix leaflet marker

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,27 +5,31 @@ import { Marker } from "react-leaflet/Marker"
 import { LatLngExpression } from "leaflet"
 import { FC } from "react"
 import { styled } from "styled-components"
+import Leaflet from 'leaflet'
 
 type MapProps = {
   position: LatLngExpression
 }
 
-const Map: FC<MapProps> = ({ position }) => (
-  <Container>
-    <MapContainer
-      center={position}
-      zoom={16}
-      scrollWheelZoom={false}
-      style={{ width: "400px", height: "300px" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position} />
-    </MapContainer>
-  </Container>
-)
+const Map: FC<MapProps> = ({ position }) => {
+  const icon = Leaflet.icon({ iconUrl: "/marker-icon.png" })
+  return (
+    <Container>
+      <MapContainer
+        center={position}
+        zoom={16}
+        scrollWheelZoom={false}
+        style={{ width: "400px", height: "300px" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position} icon={icon} />
+      </MapContainer>
+    </Container>
+  )
+}
 
 export default Map
 
